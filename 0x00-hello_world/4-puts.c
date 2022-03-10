64fd60ef49ce77e990b7751a10c085e7a4609729
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-09 23:11:31 -0800

    write c programming that print exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/*
+ *main entry point
+ *
+ *
+ *print exactly programming is like building a multilingual puzzle using puts function
+ *
+ */
+
 int  main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
