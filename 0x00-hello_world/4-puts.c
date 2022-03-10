650e8d4f211ac6cab8632c8495495b5aee1e0667
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-09 23:00:25 -0800

    write c program that print exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int  main(){
-  printf("\"Programming is like building a multilingual puzzle");
+  puts("\"Programming is like building a multilingual puzzle");
   return (0);
     }
