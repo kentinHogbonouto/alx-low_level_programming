3f24041362622b9574463f29eda934ae41661b68
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-17 13:24:08 -0700

    update

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _isupper(int l);
+int _isdigit(int x);
+int mul(int w, int y);
+int _putchar(char x);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int s);
+void print_diagonal(int s);
+void print_square(int s);
+void print_triangle(int s);
+void print_number(int s);
+#endif
