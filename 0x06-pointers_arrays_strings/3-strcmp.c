041c9af46557343f64f14ec74e400391852752cd
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-23 16:10:40 -0700

    strcmp function created

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strmcp - function declaration
+ *@s1: parameter 1
+ *@s2: parameter 2
+ *
+ *Return: always 0
+ */
+
+int _strmcp(char *s1, char *s2)
+{
+while (s1 != '\0' && (*s1 == *s2))
+{
+s1++;
+s2++;
+}
+if (s1 == s2)
+{
+return (0);
+}
+else
+{
+return ((int)*s1 - (int)*s2);
+}
+}
