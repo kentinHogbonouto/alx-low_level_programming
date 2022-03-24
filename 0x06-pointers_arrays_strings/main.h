0f2edb0f3102e20715dd49513e1a52a2b4c692ca
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-24 00:12:35 -0700

    update

--- 0x06-pointers_arrays_strings/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-#endif /*MAIN_H*/
