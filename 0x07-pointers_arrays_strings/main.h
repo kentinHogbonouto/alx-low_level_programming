31a421f1eae95112c097ada0c7e3779d1796957e
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-28 04:17:26 -0700

    updated

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 #include <stdio.h>
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
-
-#endif /*MAIN_H*/
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+#endif /* MAIN_H */
