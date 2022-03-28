f3507ab877dc41d1936ee8246db3540de9715609
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-28 03:42:52 -0700

    mem file created

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memeset - function declaration
+ *@s: First parameter
+ *@b: second parameter
+ *@n: third parameter
+ *
+ *Return: always 0
+ */
+
+char *_memeset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+n--
+}
+return (s);
+}
