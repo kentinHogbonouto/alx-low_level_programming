b71522f7657aa001951af3064c668f7ab4800ace
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-29 04:35:02 -0700

    updated main file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void set_string(char **s, char *to);
 #endif /* MAIN_H */
