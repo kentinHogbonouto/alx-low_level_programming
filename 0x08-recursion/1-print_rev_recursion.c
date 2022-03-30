fbd4f647a85d7893c633cb2e5cdc65b774819bb2
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-30 05:25:16 -0700

    print rev file created

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function
+ *
+ * @s: the chaine
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
