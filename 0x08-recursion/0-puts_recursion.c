e7529355c7aa9ddf4403d72413d4ab2bf0be4f7f
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-03-30 05:16:12 -0700

    0 puts recursion file created

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function
+ *
+ * @s: the chaine
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+}
