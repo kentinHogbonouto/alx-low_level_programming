d0576787da45fc0ada8489363bde20e57f53c7af
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-04-01 05:45:44 -0700

    mul file created

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+ * ft_atoi - function
+ *
+ * @str: the chaine
+ * Return: Always 0.
+ */
+intft_atoi(const char *str)
+{
+intn;
+intsign;
+n = 0;
+while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\r' ||
+	 
+	 *str == '\n' || *str == '\f')
+str++;
+sign = *str == '-' ? -1 : 1;
+if (sign == -1 || *str == '+')
+str++;
+while (*str)
+{
+if (*str >= '0' && *str <= '9')
+n = n * 10 + (*str - '0');
+else
+break;
+str++;
+}
+return (n * sign);
+}
+/**
+ * main - function
+ *
+ * @argv: the chaine of chaine
+ * @argc: the int
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+UNUSED(argc);
+if (argc != 3)
+{
+printf("Error\n");
+return (0);
+}
+printf("%d\n", ft_atoi(argv[1]) * ft_atoi(argv[2]));
+return (0);
+}
