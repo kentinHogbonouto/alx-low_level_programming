55c1eb9848227c908f6f93ffc95f35b698cd28d6
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-04-01 05:00:47 -0700

    args 2 file created

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - function declaration
+ *@argv: arrays of arguments
+ *@argc: argument counter
+ *Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+int i;
+for(i = 0; i < argc; i++)
+printf("%s\n", argv[i];
+return (0);
+}
