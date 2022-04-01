e23c46844b6b750600967514554d4c31f164a299
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-04-01 05:05:36 -0700

    update 2-args file

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- *main - function declaration
- *@argv: arrays of arguments
- *@argc: argument counter
- *Return: always 0
+ * main - function
+ *
+ * @argv: the chaine of chaine
+ * @argc: the int
+ * Return: Always 0.
  */
-
 int main(int argc, char **argv)
 {
 int i;
-for(i = 0; i < argc; i++)
-printf("%s\n", argv[i];
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
 return (0);
 }
+
