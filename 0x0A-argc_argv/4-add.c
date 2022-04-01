b16bedc3d11095a21742c55fc469c3e4c62023dc
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-04-01 06:29:24 -0700

    updated add script file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,59 +1,27 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * ft_atoi - function
- *
- * @str: the chaine
- * Return: Always 0.
+ *main - adds positive numbers
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Return: 0 on success, 1 on failure
  */
-
-intft_atoi(const char *str)
+int main(int argc, char *argv[])
 {
-intn;
-intsign;
-n = 0;
-while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\r' ||*str == '\n' || *str == '\f')
-str++;
-sign = *str == '-' ? -1 : 1;
-if (sign == -1 || *str == '+')
-str++;
-while (*str)
-{
-if (*str >= '0' && *str <= '9')
-n = n * 10 + (*str - '0');
-else
-{
-printf("Error\n");
-exit(EXIT_SUCCESS);
-/*break*/
-}
-str++;
-}
-return (n * sign);
-}
-
-/**
- * main - function
- *
- * @argv: the chaine of chaine
- * @argc: the int
- * Return: Always 0.
- */
-int main(int argc, char **argv)
-{
-int i, som = 0, a;
-if (argc == 1)
-{
-printf("0\n");
-return (0);
-}
+int i, j, sum = 0;
 for (i = 1; i < argc; i++)
 {
-a = ft_atoi(argv[i]);
-if (a >= 0)
-som = som + a;
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
 }
-printf("%d\n", som);
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
 return (0);
 }
