2b7d986c8bb8c5dd14d956554466deb28efdee4f
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2022-11-03 00:58:53 +0100

    Fix bugs

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,43 +1,8 @@
-<<<<<<< HEAD
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-		int n;
-		size_t index;
-		struct listint_s *next;
-} listint_t;
-
-/**
- * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- */
-typedef struct skiplist_s
-{
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
-} skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -50,11 +15,3 @@ skiplist_t *linear_skip(skiplist_t *list, int value);
 
 
 #endif /* SEARCH_ALGO_H */
-=======
-#ifndef LINEAR_SEARCH_ALGO_H
-#define LINEAR_SEARCH_ALGO_H
-
-int linear_search(int *array, size_t size, int value);
-
-#endif /*LINEAR_SEARCH_ALGO_H*/
->>>>>>> 2a87ddbd312b39733112ea1c900c711fb10fd0f1
